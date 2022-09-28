a0fe6d849e43b9ab1611dc9da7ec9767bc525d81
deborabuzatto <deborabuzatto27@gmail.com>
2022-09-28 14:40:33 -0300

    qqqqqqqq

---
+++ src/services/action.php
@@ -0,0 +1,22 @@
+<?php
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$mensagem = $_POST['message'];
+
+$arquivo = "
+    <html>
+        <p><b>Nome: </b>$nome</p>
+        <p><b>E-mail: </b>$email</p>
+        <p><b>Mensagem: </b>$mensagem</p>
+    </html>
+";
+
+$destino = "deborabuzatto27@gmail.com";
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+
+mail($destino, $arquivo, $headers);
+header("Location: index.html");
+
+?>
